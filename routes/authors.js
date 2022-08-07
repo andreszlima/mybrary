4d111e4b6a18eb37dbcfb9bedbb0b59d5ae7fe26
andreszlima <andre.szlima1@gmail.com>
2022-08-07 10:39:03 -0300

    added search and index

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All authors route
-router.get('/', (req,res) => {
-    res.render('authors/index')
+router.get('/', async (req,res) => {
+    let searchOptions = {}
+    if (req.query.name != null || req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        console.log('Deu bom')
+        res.render('authors/index', { 
+            authors: authors, 
+            searchOptions: req.query
+        })
+    } catch (error) {
+        console.error('Deu erro')
+        res.redirect('/')
+    }
 })
 
 // New author route
 router.get('/new', (req,res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create author route
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name,
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
 })
 
-module.exports = routerAuthors
+module.exports = router
--- server.js
+++ server.js
@@ -6,23 +6,26 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
+
+// Connect to mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-// Connect to mongoose
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true })
-
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 
 app.use('/', indexRouter)
+app.use('/authors/', authorsRouter)
 
 app.listen(process.env.PORT || 3000)
